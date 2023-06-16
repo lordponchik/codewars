5d694367b038a82946ce4992bd807c9806321146
Denys Strashniuk <strashnyuk.denis@icloud.com>
2023-06-16 21:03:15 +0200

    add task-17

---
+++ script/task-17.js
@@ -0,0 +1,17 @@
+/*8 kyu №17
+
+Convert boolean values to strings 'Yes' or 'No'.
+Complete the method that takes a boolean value and return 
+a "Yes" string for true, or a "No" string for false.
+
+
+*/
+
+function boolToWord(bool) {
+  //...
+
+  return bool ? 'Yes' : 'No';
+}
+
+console.log(boolToWord('man i need a taxi up to ubud')); // 'nam i deen a ixat pu ot dubu'
+console.log(boolToWord('The quick brown fox jumps over the lazy dog.')); //''ehT kciuq nworb xof spmuj revo eht yzal .god')'
