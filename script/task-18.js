6c18f5fd1e818193da472557f108fa18d3972522
Denys Strashniuk <strashnyuk.denis@icloud.com>
2023-06-16 21:10:57 +0200

    add task-18

---
+++ script/task-18.js
@@ -0,0 +1,29 @@
+/*8 kyu №18
+
+Beginner Series #2 Clock
+Clock shows h hours, m minutes and s seconds after midnight.
+
+Your task is to write a function which returns 
+the time since midnight in milliseconds.
+Example:
+h = 0
+m = 1
+s = 1
+
+result = 61000
+Input constraints:
+
+0 <= h <= 23
+0 <= m <= 59
+0 <= s <= 59
+
+
+*/
+
+function past(h, m, s) {
+  //#Happy Coding! ^_^
+  return (h * 60 * 60 + m * 60 + s) * 1000;
+}
+
+console.log(past(0, 1, 1)); // 61000
+console.log(past(1, 1, 1)); //3661000
