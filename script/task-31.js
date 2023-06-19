e5e513029c43c1cfde002d732ae4bfb7480b438d
Denys Strashniuk <strashnyuk.denis@icloud.com>
2023-06-19 19:24:52 +0200

    add task-31

---
+++ script/task-31.js
@@ -0,0 +1,13 @@
+/*8 kyu №31
+Returning Strings
+Make a function that will return a greeting statement that uses an input; your program should return, "Hello, <name> how are you doing today?".
+
+[Make sure you type the exact thing I wrote or the program may not execute properly]
+*/
+
+function greet(name) {
+  //your code here
+  return `Hello, ${name} how are you doing today?`;
+}
+
+console.log(greet('Ryan')); // Hello, Ryan how are you doing today?
