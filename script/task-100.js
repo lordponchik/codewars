a5a0a178870dfec757e547b537b3c6dd5877c38e
Denys Strashniuk <strashnyuk.denis@icloud.com>
2023-07-09 22:13:34 +0200

    fix task-100

--- script/task-100.js
+++ script/task-100.js
@@ -1,16 +1,72 @@
-/*8 kyu №98
-Parse nice int from char problem
-You ask a small girl,"How old are you?" She always says, "x years old", where x is a random number between 0 and 9.
+/*8 kyu №100
+Expressions Matter
+Task
+Given three integers a ,b ,c, return the largest number obtained after inserting the following operators and brackets: +, *, ()
+In other words , try every combination of a,b,c with [*+()] , and return the Maximum Obtained (Read the notes for more detail about it)
+Example
+With the numbers are 1, 2 and 3 , here are some ways of placing signs and brackets:
 
-Write a program that returns the girl's age (0-9) as an integer.
+1 * (2 + 3) = 5
+1 * 2 * 3 = 6
+1 + 2 * 3 = 7
+(1 + 2) * 3 = 9
+So the maximum value that you can obtain is 9.
 
-Assume the test input string is always a valid string. For example, the test input may be "1 year old" or "5 years old". The first character in the string is always a number.
+Notes
+The numbers are always positive.
+The numbers are in the range (1  ≤  a, b, c  ≤  10).
+You can use the same operation more than once.
+It's not necessary to place all the signs and brackets.
+Repetition in numbers may occur .
+You cannot swap the operands. For instance, in the given example you cannot get expression (1 + 3) * 2 = 8.
+Input >> Output Examples:
+expressionsMatter(1,2,3)  ==>  return 9
+Explanation:
+After placing signs and brackets, the Maximum value obtained from the expression (1+2) * 3 = 9.
 
-FUNDAMENTALS
+expressionsMatter(1,1,1)  ==>  return 3
+Explanation:
+After placing signs, the Maximum value obtained from the expression is 1 + 1 + 1 = 3.
+
+expressionsMatter(9,1,1)  ==>  return 18
+Explanation:
+After placing signs and brackets, the Maximum value obtained from the expression is 9 * (1+1) = 18. ___# Task
+
+Given three integers a ,b ,c, return the largest number obtained after inserting the following operators and brackets: +, *, ()
+In other words , try every combination of a,b,c with [*+()] , and return the Maximum Obtained
+Example
+With the numbers are 1, 2 and 3 , here are some ways of placing signs and brackets:
+
+1 * (2 + 3) = 5
+1 * 2 * 3 = 6
+1 + 2 * 3 = 7
+(1 + 2) * 3 = 9
+So the maximum value that you can obtain is 9.
+
+Notes
+The numbers are always positive.
+The numbers are in the range (1  ≤  a, b, c  ≤  10).
+You can use the same operation more than once.
+It's not necessary to place all the signs and brackets.
+Repetition in numbers may occur .
+You cannot swap the operands. For instance, in the given example you cannot get expression (1 + 3) * 2 = 8.
+Input >> Output Examples:
+expressionsMatter(1,2,3)  ==>  return 9
+Explanation:
+After placing signs and brackets, the Maximum value obtained from the expression (1+2) * 3 = 9.
+
+expressionsMatter(1,1,1)  ==>  return 3
+Playing with Numbers Series
+Playing With Lists/Arrays Series
+Bizarre Sorting-katas
+For More Enjoyable Katas
+ALL translations are welcomed
+Enjoy Learning !!
+Zizou
+ALGORITHMSFUNDAMENTALS
 */
-function getAge(inputString) {
-  // return the girl's correct age as an integer. Happy coding :)
-  return Number(inputString[0]);
+function expressionMatter(a, b, c) {
+  return Math.max(...[(a + b) * c, a * (b + c), a * b * c, a + b * c, a + b + c]); // highest achievable result
 }
 
-console.log(getAge('4 years old')); //4
+console.log(expressionMatter(1, 2, 3)); //9
