5c9e7878d07a584431b36c1cafd703422ea497ae
Denys Strashniuk <strashnyuk.denis@icloud.com>
2023-07-14 22:28:22 +0200

    add task-124

---
+++ script/task-124.js
@@ -0,0 +1,12 @@
+/*8 kyu №124
+Bin to Decimal
+Complete the function which converts a binary number (given as a string) to a decimal number.
+*/
+
+function binToDec(bin) {
+  // ...
+
+  return parseInt(bin, 2);
+}
+
+console.log(binToDec('1001001')); // 73
