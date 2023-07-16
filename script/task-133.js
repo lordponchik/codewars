b369f6b3886a2948fa653df294ec93f705c614de
Denys Strashniuk <strashnyuk.denis@icloud.com>
2023-07-16 12:26:51 +0200

    add task-133

---
+++ script/task-133.js
@@ -0,0 +1,32 @@
+/*6 kyu №133
+6 kyu
+Build a pile of Cubes
+Your task is to construct a building which will be a pile of n cubes. The cube at the bottom will have a volume of 
+n^3, the cube above will have volume of (n−1)^3 and so on until the top which will have a volume of 1^3.
+
+You are given the total volume m of the building. Being given m can you find the number n of cubes you will have to build?
+The parameter of the function findNb (find_nb, find-nb, findNb, ...) will be an integer m and you have to return the integer n such as 
+n^3+(n-1)^3+(n-2)^3+...+1^3=m if such a n exists or -1 if there is no such n.
+
+Examples:
+findNb(1071225) --> 45
+
+findNb(91716553919377) --> -1
+
+MATHEMATICSALGORITHMS
+*/
+
+function findNb(m) {
+  // your code
+  let result = 0;
+  let step = 1;
+
+  for (step; result < m; step += 1) {
+    result += step ** 3;
+  }
+
+  return result === m ? step - 1 : -1;
+}
+
+console.log(findNb(1071225)); //45
+console.log(findNb(24723578342962)); //-1
