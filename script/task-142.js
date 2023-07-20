704b603296f85f8a3fab85ee63b490368f87527e
Denys Strashniuk <strashnyuk.denis@icloud.com>
2023-07-20 10:37:33 +0200

    add task-142

---
+++ script/task-142.js
@@ -0,0 +1,19 @@
+/*8 kyu №142
+
+5 without numbers !!
+Write a function that always returns 5
+
+Sounds easy right? Just bear in mind that you can't use any of the following characters: 0123456789*+-/
+
+Good luck :)
+
+RESTRICTEDFUNDAMENTALSPUZZLES
+
+*/
+
+// complete the function
+function unusualFive() {
+  return '_five'.length;
+}
+
+console.log(unusualFive()); //5
