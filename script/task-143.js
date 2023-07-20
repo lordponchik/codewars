e85e938521934a2934e4855805ddb6e83f92db97
Denys Strashniuk <strashnyuk.denis@icloud.com>
2023-07-20 10:46:57 +0200

    add task-143

---
+++ script/task-143.js
@@ -0,0 +1,13 @@
+/*8 kyu №143
+
+MakeUpperCase
+Write a function which converts the input string to uppercase.
+*/
+
+// complete the function
+function makeUpperCase(str) {
+  // Code here
+  return str.toUpperCase();
+}
+
+console.log(makeUpperCase('hello')); //"HELLO"
