67f8bc1aebfec621c116f455a23ea9cf3ba7fe03
Denys Strashniuk <strashnyuk.denis@icloud.com>
2023-07-23 16:12:15 +0200

    add task-157

---
+++ script/task-157.js
@@ -0,0 +1,19 @@
+/*8 kyu №157
+
+Reversed sequence
+Build a function that returns an array of integers from n to 1 where n>0.
+
+Example : n=5 --> [5,4,3,2,1]
+*/
+
+const reverseSeq = n => {
+  const arr = [];
+
+  for (let i = n; i > 0; i -= 1) {
+    arr.push(i);
+  }
+
+  return arr;
+};
+
+console.log(reverseSeq(5)); // [5,4,3,2,1]
