59b4b1294a2f8ce9fe9336e603b5b98bdbb8600a
Denys Strashniuk <strashnyuk.denis@icloud.com>
2023-07-23 16:45:14 +0200

    add task-160

---
+++ script/task-160.js
@@ -0,0 +1,12 @@
+/*7 kyu №160
+
+Sum of angles
+Find the total sum of internal angles (in degrees) in an n-sided simple polygon. N will be greater than 2.
+*/
+
+function angle(n) {
+  //code here
+  return 180(n - 2);
+}
+
+console.log(angle(3)); // 180
