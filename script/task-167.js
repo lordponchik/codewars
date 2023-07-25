f6734433bc661421632e673058dd0a244be929c0
Denys Strashniuk <strashnyuk.denis@icloud.com>
2023-07-25 08:08:38 +0200

    add task-167

---
+++ script/task-167.js
@@ -0,0 +1,56 @@
+/*6 kyu №167
+
+Replace With Alphabet Position
+Welcome.
+
+In this kata you are required to, given a string, replace every letter with its position in the alphabet.
+
+If anything in the text isn't a letter, ignore it and don't return it.
+
+"a" = 1, "b" = 2, etc.
+
+Example
+alphabetPosition("The sunset sets at twelve o' clock.")
+Should return "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11" ( as a string )
+
+STRINGSFUNDAMENTALS
+*/
+
+function alphabetPosition(text) {
+  const arr_en = [
+    'a',
+    'b',
+    'c',
+    'd',
+    'e',
+    'f',
+    'g',
+    'h',
+    'i',
+    'j',
+    'k',
+    'l',
+    'm',
+    'n',
+    'o',
+    'p',
+    'q',
+    'r',
+    's',
+    't',
+    'u',
+    'v',
+    'w',
+    'x',
+    'y',
+    'z',
+  ];
+
+  return text
+    .toLowerCase()
+    .split('')
+    .filter(el => arr_en.includes(el))
+    .map(el => arr_en.indexOf(el) + 1)
+    .join(' ');
+}
+console.log(alphabetPosition("The sunset sets at twelve o'clock")); //"20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11"
