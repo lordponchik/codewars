1d2c66faf6f31d10498fa144c8d55db98c0f2e8b
Denys Strashniuk <strashnyuk.denis@icloud.com>
2023-07-25 08:16:15 +0200

    add task-168

---
+++ script/task-168.js
@@ -0,0 +1,26 @@
+/*8 kyu №168
+
+Grasshopper - Messi goals function
+Messi goals function
+Messi is a soccer player with goals in three leagues:
+
+LaLiga
+Copa del Rey
+Champions
+Complete the function to return his total number of goals in all three leagues.
+
+Note: the input will always be valid.
+
+For example:
+
+5, 10, 2  -->  17
+FUNDAMENTALS
+*/
+
+function goals(laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
+  // code goes here
+
+  return laLigaGoals + copaDelReyGoals + championsLeagueGoals;
+}
+
+console.log(goals(5, 10, 2)); //17
