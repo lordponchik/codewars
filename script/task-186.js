339704637c4e07fe47040ff2713a437874c24d34
Denys Strashniuk <strashnyuk.denis@icloud.com>
2023-07-31 09:36:21 +0200

    add task-186

---
+++ script/task-186.js
@@ -0,0 +1,15 @@
+/*8 kyu №186
+
+Function 2 - squaring an argument
+Now you have to write a function that takes an argument and returns the square of it.
+
+FUNDAMENTALS
+
+*/
+
+function square(num) {
+  // put your code here
+  return Math.pow(num, 2);
+}
+
+console.log(squaring(5)); // 25
