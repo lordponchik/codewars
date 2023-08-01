b555a1c4bdc38d3c9bcb037acbca6c4cd31f26b2
Denys Strashniuk <strashnyuk.denis@icloud.com>
2023-08-01 18:08:17 +0200

    add task-191

---
+++ script/task-191.js
@@ -0,0 +1,18 @@
+/*8 kyu №191
+Opposite number
+Very simple, given an integer or a floating-point number, find its opposite.
+
+Examples:
+
+1: -1
+14: -14
+-34: 34
+FUNDAMENTALS
+*/
+
+function opposite(number) {
+  //your code here
+  return (number *= -1);
+}
+
+console.log(opposite(-34)); // 34
