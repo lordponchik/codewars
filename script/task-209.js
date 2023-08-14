2c538bd5d2c1008f0b9b49361f6969ba7d906ac4
Denys Strashniuk <strashnyuk.denis@icloud.com>
2023-08-14 21:49:21 +0200

    add task-209

---
+++ script/task-209.js
@@ -0,0 +1,16 @@
+/*8 kyu №209
+Grasshopper - Function syntax debugging
+Grasshopper - Function syntax debugging
+A student was working on a function and made some syntax mistakes while coding. Help them find their mistakes and fix them.
+
+FUNDAMENTALS
+
+function main [verb, noun]
+  return verb + noun
+}
+*/
+function main(verb, noun) {
+  return verb + noun;
+}
+
+console.log(main('take ', 'item')); // 'take item'
