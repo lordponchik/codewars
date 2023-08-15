eeec4df441f02d6e9a7788ca5f5ba7c497bb1e63
Denys Strashniuk <strashnyuk.denis@icloud.com>
2023-08-15 15:59:54 +0200

    add task-214

---
+++ script/task-214.js
@@ -0,0 +1,36 @@
+/*8 kyu №214
+simple calculator
+You are required to create a simple calculator that returns the result of addition, subtraction, multiplication or division of two numbers.
+
+Your function will accept three arguments:
+The first and second argument should be numbers.
+The third argument should represent a sign indicating the operation to perform on these two numbers.
+
+if the variables are not numbers or the sign does not belong to the list above a message "unknown value" must be returned.
+
+Example:
+calculator(1,2,"+"); //=> result will be 3
+calculator(1,2,"&"); //=> result will be "unknown value"
+calculator(1,"k","*"); //=> result will be "unknown value"
+Good luck!
+
+FUNDAMENTALS
+*/
+function calculator(a, b, sign) {
+  if (typeof a !== 'number' || typeof b !== 'number') return 'unknown value';
+
+  switch (sign) {
+    case '+':
+      return a + b;
+    case '-':
+      return a - b;
+    case '*':
+      return a * b;
+    case '/':
+      return a / b;
+    default:
+      return 'unknown value';
+  }
+}
+
+console.log(calculator(5, 7, '+')); // 12
