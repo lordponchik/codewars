ca194623dca561581a6337f90ecca051e5c811bc
Denys Strashniuk <strashnyuk.denis@icloud.com>
2023-08-15 15:41:53 +0200

    add task-212

---
+++ script/task-212.js
@@ -0,0 +1,27 @@
+/*8 kyu №212
+Define a card suit
+You get any card as an argument. Your task is to return the suit of this card (in lowercase).
+
+Our deck (is preloaded):
+
+('3♣') -> return 'clubs'
+('3♦') -> return 'diamonds'
+('3♥') -> return 'hearts'
+('3♠') -> return 'spades'
+FUNDAMENTALSSTRINGS
+*/
+function defineSuit(card) {
+  // good luck
+  switch (card[card.length - 1]) {
+    case '♣':
+      return 'clubs';
+    case '♦':
+      return 'diamonds';
+    case '♥':
+      return 'hearts';
+    case '♠':
+      return 'spades';
+  }
+}
+
+console.log(defineSuit('3♣')); // "clubs"
