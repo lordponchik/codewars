a43a01226c9371f91ef06741bef4a5f0342371ec
Denys Strashniuk <strashnyuk.denis@icloud.com>
2023-08-21 10:04:16 +0200

    add task-228

---
+++ script/task-228.js
@@ -0,0 +1,24 @@
+/*8 kyu №228
+Regex count lowercase letters
+Your task is simply to count the total number of lowercase letters in a string.
+
+Examples
+"abc" ===> 3
+
+"abcABC123" ===> 3
+
+"abcABC123!@€£#$%^&*()_-+=}{[]|\':;?/>.<,~" ===> 3
+
+"" ===> 0;
+
+"ABC123!@€£#$%^&*()_-+=}{[]|\':;?/>.<,~" ===> 0
+
+"abcdefghijklmnopqrstuvwxyz" ===> 26
+FUNDAMENTALSREGULAR EXPRESSIONSALGORITHMS
+*/
+function lowercaseCount(str) {
+  //How many?
+  return str.split('').filter(el => el.toUpperCase() !== el).length;
+}
+
+console.log(lowercaseCount("abcABC123!@€£#$%^&*()_-+=}{[]|':;?/>.<,~")); //3
