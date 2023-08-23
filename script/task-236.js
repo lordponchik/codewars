04f64587fb6933121527f68e7c99acaf323fb5d9
Denys Strashniuk <79088186+lordponchik@users.noreply.github.com>
2023-08-23 09:16:46 +0200

    add task-236

---
+++ script/task-236.js
@@ -0,0 +1,16 @@
+/*8 kyu №236
+Grasshopper - Debug sayHello
+Debugging sayHello function
+The starship Enterprise has run into some problem when creating a program to greet everyone as they come aboard. It is your job to fix the code and get the program working again!
+
+Example output:
+
+Hello, Mr. Spock
+FUNDAMENTALS
+*/
+function sayHello(name) {
+  return `Hello, ${name}`;
+}
+
+console.log(checkCoupon("123", "123", "July 9, 2015", "July 9, 2015")); //true
+console.log(checkCoupon("123", "123", "July 9, 2015", "July 2, 2015")); // false
