1d51b4f6383cd5d148b2715d36ee791063ac0010
Denys Strashniuk <79088186+lordponchik@users.noreply.github.com>
2023-08-26 10:28:57 +0200

    add task-248

---
+++ script/task-248.js
@@ -0,0 +1,33 @@
+/*
+6 kyu №248
+Sum of Digits / Digital Root
+Digital root is the recursive sum of all the digits in a number.
+
+Given n, take the sum of the digits of n. If that value has more than one digit, continue reducing in this way until a single-digit number is produced. The input will be a non-negative integer.
+
+Examples
+    16  -->  1 + 6 = 7
+   942  -->  9 + 4 + 2 = 15  -->  1 + 5 = 6
+132189  -->  1 + 3 + 2 + 1 + 8 + 9 = 24  -->  2 + 4 = 6
+493193  -->  4 + 9 + 3 + 1 + 9 + 3 = 29  -->  2 + 9 = 11  -->  1 + 1 = 2
+MATHEMATICSALGORITHMS
+*/
+
+function digitalRoot(n) {
+  // ...
+  let value = n;
+
+  while (value.toString().length > 1) {
+    value = value
+      .toString()
+      .split('')
+      .reduce((sum, el) => {
+        return (sum += Number(el));
+      }, 0);
+  }
+
+  return value;
+}
+
+console.log(digitalRoot(16)); //7
+console.log(digitalRoot(942)); //6
