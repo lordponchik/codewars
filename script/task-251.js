a8837095c1ad8e01a98a55d2cf68ec13fab9f877
Denys Strashniuk <79088186+lordponchik@users.noreply.github.com>
2023-08-27 21:47:15 +0200

    add task-251

---
+++ script/task-251.js
@@ -0,0 +1,21 @@
+/*
+8 kyu №251
+8 kyu
+My head is at the wrong end!
+You're at the zoo... all the meerkats look weird. Something has gone terribly wrong - someone has gone and switched their heads and tails around!
+
+Save the animals by switching them back. You will be given an array which will have three values (tail, body, head). It is your job to re-arrange the array so that the animal is the right way round (head, body, tail).
+
+Same goes for all the other arrays/lists that you will get in the tests: you have to change the element positions with the same exact logics
+
+Simples!
+
+ARRAYSLISTSALGORITHMS
+*/
+
+function fixTheMeerkat(arr) {
+  //your code here
+  return arr.reverse();
+}
+
+console.log(fixTheMeerkat(['tail', 'body', 'head'])); // ["head", "body", "tail"]
