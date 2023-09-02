6de46770628247ac59aab71412edb615e09e1ea2
Denys Strashniuk <79088186+lordponchik@users.noreply.github.com>
2023-09-02 09:48:22 +0200

    add task-260

---
+++ script/task-260.js
@@ -0,0 +1,52 @@
+/*
+6 kyu №260
+
+CamelCase Method
+Write a method (or function, depending on the language) that converts a string to camelCase, that is, all words must have their first letter capitalized and spaces must be removed.
+
+Examples (input --> output):
+"hello case" --> "HelloCase"
+"camel case word" --> "CamelCaseWord"
+Don't forget to rate this kata! Thanks :)
+
+FUNDAMENTALSALGORITHMSSTRINGS
+*/
+
+String.prototype.camelCase = function () {
+  //your code here
+  const obj_en = {
+    a: 'A',
+    b: 'B',
+    c: 'C',
+    d: 'D',
+    e: 'E',
+    f: 'F',
+    g: 'G',
+    h: 'H',
+    i: 'I',
+    j: 'J',
+    k: 'K',
+    l: 'L',
+    m: 'M',
+    n: 'N',
+    o: 'O',
+    p: 'P',
+    q: 'Q',
+    r: 'R',
+    s: 'S',
+    t: 'T',
+    u: 'U',
+    v: 'V',
+    w: 'W',
+    x: 'X',
+    y: 'Y',
+    z: 'Z',
+  };
+
+  return this.toLowerCase()
+    .split(' ')
+    .map(el => el.replace(el[0], obj_en[el[0]]))
+    .join('');
+};
+
+console.log('test case'.camelCase()); // "TestCase"
