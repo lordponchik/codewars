666de3ab72133dd319d0c48280b7ebe9e1848c16
Denys Strashniuk <79088186+lordponchik@users.noreply.github.com>
2023-09-08 21:05:44 +0200

    add task-274

---
+++ script/task-274.js
@@ -0,0 +1,23 @@
+/*
+Printing Array elements with Comma delimiters
+Input: Array of elements
+
+["h","o","l","a"]
+
+Output: String with comma delimited elements of the array in th same order.
+
+"h,o,l,a"
+
+Note: if this seems too simple for you try the next level
+
+ARRAYSFUNDAMENTALS
+*/
+
+// write the function isAnagram
+
+function printArray(array) {
+  //show me the code!
+  return array.join(',');
+}
+
+console.log(printArray(['h', 'o', 'l', 'a'])); //"h,o,l,a"
