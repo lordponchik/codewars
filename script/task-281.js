1e76a4d8f5b416f8578d5c25ecfc2a2bb6a5464f
Denys Strashniuk <79088186+lordponchik@users.noreply.github.com>
2023-10-01 07:42:34 +0200

    add task-281

---
+++ script/task-281.js
@@ -0,0 +1,14 @@
+/*
+8 kyu 281
+Kata Example Twist
+This is an easy twist to the example kata (provided by Codewars when learning how to create your own kata).
+
+Add the value "codewars" to the array websites 1,000 times.
+*/
+
+// add the value "codewars" to the websites array 1,000 times
+var websites = [];
+websites.length = 1000;
+websites.fill('codewars');
+
+console.log(websites);
