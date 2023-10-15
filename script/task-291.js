b4693b24a239ef58180296890a560824e7c1eeea
Denys Strashniuk <79088186+lordponchik@users.noreply.github.com>
2023-10-15 14:21:23 +0200

    add task-291

---
+++ script/task-291.js
@@ -0,0 +1,23 @@
+/*
+8 kyu 291
+pick a set of first elements
+Write a function to get the first element(s) of a sequence. Passing a parameter n (default=1) will return the first n element(s) of the sequence.
+
+If n == 0 return an empty sequence []
+
+Examples
+var arr = ['a', 'b', 'c', 'd', 'e'];
+first(arr) //=> ['a'];
+first(arr, 2) //=> ['a', 'b']
+first(arr, 3) //=> ['a', 'b', 'c'];
+first(arr, 0) //=> [];
+ARRAYSFUNDAMENTALS
+*/
+
+function first(arr, n = 1) {
+  // return arr;
+  return [...arr].splice(0, n);
+}
+
+console.log(first(['a', 'b', 'c', 'd', 'e'], 2)); //['a', 'b']
+console.log(first(['a', 'b', 'c', 'd', 'e'], 0)); //[]
