0df4fd3a2c2ff93c67c6b0023da9804e997a08ea
Denys Strashniuk <79088186+lordponchik@users.noreply.github.com>
2023-10-15 14:07:04 +0200

    add task-290

---
+++ script/task-290.js
@@ -0,0 +1,31 @@
+/*
+8 kyu 290
+Classy Classes
+Classy Classes
+Basic Classes, this kata is mainly aimed at the new JS ES6 Update introducing classes
+
+Task
+Your task is to complete this Class, the Person class has been created. You must fill in the Constructor method to accept a name as string and an age as number, complete the get Info property and getInfo method/Info getter which should return johns age is 34
+
+Reference: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Classes
+
+FUNDAMENTALSOBJECT-ORIENTED PROGRAMMING
+
+class Person {
+  constructor() {
+  
+  }
+}
+*/
+
+class Person {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+    this.info = `${this.name}s age is ${this.age}`;
+  }
+}
+
+const john = new Person('john', 34);
+
+console.log(john.info); //"johns age is 34"
