d484076020dd126ac5e361ed79f43163a4e1048b
Denys Strashniuk <79088186+lordponchik@users.noreply.github.com>
2023-10-26 13:23:41 +0200

    add task-325

---
+++ script/task-325.js
@@ -0,0 +1,20 @@
+/*
+8 kyu 325
+validate code with simple regex
+Basic regex tasks. Write a function that takes in a numeric code of any length. The function should check if the code begins with 1, 2, or 3 and return true if so. Return false otherwise.
+
+You can assume the input will always be a number.
+
+REGULAR EXPRESSIONSFUNDAMENTALS
+*/
+function validateCode(code) {
+  //your code here
+  return (
+    Number(code.toString()[0]) === 1 ||
+    Number(code.toString()[0]) === 2 ||
+    Number(code.toString()[0]) === 3
+  );
+}
+console.log(validateCode(123)); // true
+console.log(validateCode(542)); // false
+console.log(validateCode(8)); // false
