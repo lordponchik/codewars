12b041d1df7a851f857f8776ea071b2569cc224c
Denys Strashniuk <79088186+lordponchik@users.noreply.github.com>
2023-10-30 10:04:26 +0100

    add task-335

---
+++ script/task-335.js
@@ -0,0 +1,18 @@
+/*
+8 kyu 335
+Plural
+We need a simple function that determines if a plural is needed or not. It should take a number, and return true if a plural should be used with that number or false if not. This would be useful when printing out a string such as 5 minutes, 14 apples, or 1 sun.
+
+You only need to worry about english grammar rules for this kata, where anything that isn't singular (one of something), it is plural (not one of something).
+
+All values will be positive integers or floats, or zero.
+
+FUNDAMENTALS
+*/
+function plural(n) {
+  // ...
+  return n !== 1;
+}
+
+console.log(plural(1)); //false
+console.log(plural(100)); //true
