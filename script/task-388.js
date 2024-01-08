edafc9614d97b20bcf80ca1c14e0d3fb687463f2
Denys Strashniuk <79088186+lordponchik@users.noreply.github.com>
2024-01-08 09:34:20 +0100

    add task-388

---
+++ script/task-388.js
@@ -0,0 +1,19 @@
+/*
+7 kyu 388
+Cat and Mouse - Easy Version
+You will be given a string (x) featuring a cat 'C' and a mouse 'm'. The rest of the string will be made up of '.'.
+
+You need to find out if the cat can catch the mouse from it's current position. The cat can jump over three characters. So:
+
+C.....m returns 'Escaped!' <-- more than three characters between
+
+C...m returns 'Caught!' <-- as there are three characters between the two, the cat can jump.
+
+FUNDAMENTALSSTRINGS
+*/
+function catMouse(x) {
+  return [...x].filter(el => el === '.').length > 3 ? 'Escaped!' : 'Caught!';
+}
+
+console.log(catMouse('C.....m')); // 'Escaped!'
+console.log(catMouse('C...m')); // 'Caught!'
