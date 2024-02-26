6559062e52755693ba83d6b9893495041782afd1
Denys Strashniuk <79088186+lordponchik@users.noreply.github.com>
2024-02-26 15:32:06 +0100

    add task-440

---
+++ script/task-440.js
@@ -0,0 +1,33 @@
+/*
+8 kyu 439
+
+Dollars and Cents
+The company you work for has just been awarded a contract to build a payment gateway. In order to help move things along, you have volunteered to create a function that will take a float and return the amount formatting in dollars and cents.
+
+39.99 becomes $39.99
+
+The rest of your team will make sure that the argument is sanitized before being passed to your function although you will need to account for adding trailing zeros if they are missing (though you won't have to worry about a dangling period).
+
+Examples:
+
+3 needs to become $3.00
+
+3.1 needs to become $3.10
+Good luck! Your team knows they can count on you!
+
+FUNCTIONAL PROGRAMMINGSTRINGSALGORITHMS
+*/
+
+function formatMoney(amount) {
+  const fraction = amount.toString().split('.');
+
+  return fraction[1] === undefined
+    ? `$${amount}.00`
+    : fraction[1].length === 1
+    ? `${amount}0`
+    : `$${amount}`;
+}
+
+console.log(formatMoney(3)); //
+console.log(formatMoney(3.1)); //
+console.log(formatMoney(3.99)); //
