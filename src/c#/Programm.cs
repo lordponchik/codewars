28f0b88c4884e5a0ddb1f3f6ffc8288815c7d115
Denys Strashniuk <d.strashniuk@gmail.com>
2025-02-21 16:26:52 +0100

    add task-1-c#

--- src/c#/Programm.cs
+++ src/c#/Programm.cs
@@ -2,7 +2,7 @@
 {
     class Program{
         static void Main(){
-            
+            Task1.Task_1();
         }
     }
 }
--- src/c#/obj/Debug/net9.0/c#.AssemblyInfo.cs
+++ src/c#/obj/Debug/net9.0/c#.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("c#")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+946300d106ffe248ea57270245a8769c646b8709")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c0e8b3b6d9e12e744203bc7e5937b36161458044")]
 [assembly: System.Reflection.AssemblyProductAttribute("c#")]
 [assembly: System.Reflection.AssemblyTitleAttribute("c#")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
