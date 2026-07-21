1c9efa59f5cf2ec56f029d966f704d005f723f1f
lordponchik <d.strashniuk@gmail.com>
2026-07-21 21:43:56 +0200

    add: js task-492

---
+++ src/javascripts/task-492.js
@@ -0,0 +1,21 @@
+/*
+7 kyu 492
+Is n divisible by (...)?
+Create a function that checks if the first argument n is divisible by all other arguments (return true if no other arguments)
+
+Example:
+
+(6,1,3)--> true because 6 is divisible by 1 and 3
+(12,2)--> true because 12 is divisible by 2
+(100,5,4,10,25,20)--> true
+(12,7)--> false because 12 is not divisible by 7
+*/
+
+function isDivisible(n, ...args){
+  return args.every(e => n%e === 0);
+}
+
+console.log(isDivisible(6,1,3)); // true
+console.log(isDivisible(12,2)); // true
+console.log(isDivisible(100,5,4,10,25,20)); // true
+console.log(isDivisible(12,7)); // false
